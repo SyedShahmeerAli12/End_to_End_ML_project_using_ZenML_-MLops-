652afd5b670af5d793340831caeb5cd557b6ab51
Shahmeer <ashahmeer73@gmail.com>
2025-02-15 22:09:30 +0500

    pipeline ready

--- steps/ingest_data.py
+++ steps/ingest_data.py
@@ -5,7 +5,19 @@ from zenml import step
 class IngestData:
     def __init__(self , data_path : str):
         self.data_path = data_path
-        pass
     def get_data(self):
         logging.info(f"ingesting Data from {self.data_path}")
         return pd.read_csv(self.data_path)
+    
+
+@step
+def ingest_df(data_path : str) -> pd.DataFrame:
+    try:
+        Ingest_Data = IngestData(data_path)
+        df = Ingest_Data.get_data()
+        return df
+    except Exception as e:
+        logging.error("error while ingestind data" , e)
+        raise e
+     
+
